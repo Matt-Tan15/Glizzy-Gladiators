fdc75f3c585923bdaaff5f787df249c083272b41
Matthew <tanmatt804@gmail.com>
2022-02-09 18:31:04 -0500

    changed bean to hotdog

--- logic.js
+++ logic.js
@@ -1,38 +1,37 @@
 kaboom({
-	background: [ 0, 0, 0 ]
-})
+  background: [0, 0, 0],
+});
 
 const block = add([
-    rect(0, 90),
-    area(),
-    pos(285,550),
-    origin("center"),
-    solid()
+  rect(0, 90),
+  area(),
+  pos(285, 550),
+  origin("center"),
+  solid(),
 ]);
 
 const block2 = add([
   rect(0, 90),
   area(),
-  pos(1080,550),
+  pos(1080, 550),
   origin("center"),
-  solid()
-])
+  solid(),
+]);
 
-let bgImage = loadSprite("background", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/54ff7af5-2e49-471e-8efc-69c98615ab82/d128hmk-5d073adb-0821-44d1-a2bf-1e936f5b16b1.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU0ZmY3YWY1LTJlNDktNDcxZS04ZWZjLTY5Yzk4NjE1YWI4MlwvZDEyOGhtay01ZDA3M2FkYi0wODIxLTQ0ZDEtYTJiZi0xZTkzNmY1YjE2YjEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.rrEKAOowLqMQiuUOrbKG4fWCAfn71GhnUWUirLQzy-o");
+let bgImage = loadSprite(
+  "background",
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/54ff7af5-2e49-471e-8efc-69c98615ab82/d128hmk-5d073adb-0821-44d1-a2bf-1e936f5b16b1.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU0ZmY3YWY1LTJlNDktNDcxZS04ZWZjLTY5Yzk4NjE1YWI4MlwvZDEyOGhtay01ZDA3M2FkYi0wODIxLTQ0ZDEtYTJiZi0xZTkzNmY1YjE2YjEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.rrEKAOowLqMQiuUOrbKG4fWCAfn71GhnUWUirLQzy-o"
+);
 
-  let background = add([
-    sprite("background"),
-    // Make the background centered on the screen
-    pos(width() / 2, height() / 2),
-    origin("center"),
-    // Allow the background to be scaled
-    scale(1),
-    // Keep the background position fixed even when the camera moves
-    fixed()
-  ]);
-<<<<<<< HEAD
-  // Scale the background to cover the screen
+let background = add([
+  sprite("background"),
+  // Make the background centered on the screen
+  pos(width() / 2, height() / 2),
+  origin("center"),
+  // Allow the background to be scaled
+  scale(1),
+  // Keep the background position fixed even when the camera moves
+  fixed(),
+]);
 
-=======
->>>>>>> cfc25b40fb93232daacfbef990315337adbc19c7
 
--- ship.js
+++ ship.js
@@ -1,27 +1,22 @@
 loadBean();
+loadSprite('hotdog', './sprites/HotDog.png')
 
 const player = add([
-	sprite("bean"),   // sprite() component makes it render as a sprite
-	pos(680, 560),     // pos() component gives it position, also enables movement
-	rotate(0),        // rotate() component gives it rotation
-	origin("center"), // origin() component defines the pivot point (defaults to "topleft")
-    area(),
-    solid()
-])
-
-
-
+  sprite("hotdog"), // sprite() component makes it render as a sprite
+  pos(680, 560), // pos() component gives it position, also enables movement
+  rotate(0), // rotate() component gives it rotation
+  origin("center"), // origin() component defines the pivot point (defaults to "topleft")
+  area(),
+  solid(),
+]);
 
 const SPEED = 320;
 
 onKeyDown("left", () => {
-	// .move() is provided by pos() component, move by pixels per second
-	player.move(-SPEED, 0)
-})
+  // .move() is provided by pos() component, move by pixels per second
+  player.move(-SPEED, 0);
+});
 
 onKeyDown("right", () => {
-	player.move(SPEED, 0)
-})
-
-
-
+  player.move(SPEED, 0);
+});
