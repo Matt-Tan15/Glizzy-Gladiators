2bec7f6f31707a52db978155efe31d55454a992d
Matt-Tan15 <tanmatt804@gmail.com>
2022-02-09 11:43:46 -0500

    gave movement

--- ship.js
+++ ship.js
@@ -2,8 +2,19 @@ loadBean();
 
 const player = add([
 	sprite("bean"),   // sprite() component makes it render as a sprite
-	pos(680, 590),     // pos() component gives it position, also enables movement
+	pos(680, 560),     // pos() component gives it position, also enables movement
 	rotate(0),        // rotate() component gives it rotation
 	origin("center"), // origin() component defines the pivot point (defaults to "topleft")
+    solid()
 ])
 
+const SPEED = 320;
+
+onKeyDown("left", () => {
+	// .move() is provided by pos() component, move by pixels per second
+	player.move(-SPEED, 0)
+})
+
+onKeyDown("right", () => {
+	player.move(SPEED, 0)
+})
