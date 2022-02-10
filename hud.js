119ecf93b6927af3d0d989ecc0bd8efbb2ab2b84
Jarae-MacyMan <sjaraebr@gmail.com>
2022-02-10 14:33:29 -0500

    hud

--- hud.js
+++ hud.js
@@ -1,1 +1,46 @@
+const scoreHolder = add ([
+    text('score:'),
+    pos(950,20),
+    layer('ui'),
+])
 
+const score = add ([
+    text('0'),
+    pos(1250,20),
+    layer('ui'),
+    {
+        valur: 0,
+    }
+])
+
+const livesHolder = add ([
+    text('lives:'),
+    pos(110,20),
+    layer('ui'),
+])
+
+loadSprite("heart", "./sprites/heart.png");
+
+const life1 = add([
+    sprite("heart"), // sprite() component makes it render as a sprite
+    pos(400, 30), // pos() component gives it position, also enables movement
+    area(),
+    solid(),
+    "heart"
+  ]);
+
+  const life2 = add([
+    sprite("heart"), // sprite() component makes it render as a sprite
+    pos(500, 30), // pos() component gives it position, also enables movement
+    area(),
+    solid(),
+    "heart"
+  ]);
+  
+  const life3 = add([
+    sprite("heart"), // sprite() component makes it render as a sprite
+    pos(600, 30), // pos() component gives it position, also enables movement
+    area(),
+    solid(),
+    "heart"
+  ]);
