aa8379edb3b4c74138f2a729aae7711eb3edfbea
Jarae-MacyMan <sjaraebr@gmail.com>
2022-02-15 10:56:34 -0500

    start page

--- logic.js
+++ logic.js
@@ -2,7 +2,23 @@ kaboom({
   background: [0, 0, 0],
 });
 
+loadSprite("hotdog", "./sprites/newCar.png");
+loadSprite("mush", "./sprites/betterMushroom.png");
 const newRoad = loadSprite("newRoad", "./sprites/newRoad.png");
+
+
+
+scene("start", () => {
+  add([text("Welcome"), pos(width() / 2, height() / 2), origin("center")]);
+
+  onKeyPress("enter", () => {
+    go("game")
+  })
+})
+go("start");
+// logic 
+
+scene("game", () => {
 
 const START_POS = -200;
 const END_POS = height();
@@ -127,26 +143,197 @@ addLevel(
   }
 );
 
+
+const player = add([
+  sprite("hotdog"), 
+  pos(width() / 2, height() - 100), 
+  rotate(0), 
+  origin("center"),
+  area(),
+  solid(),
+  "player",
+]);
+
+const SPEED = 500;
+
+onKeyDown("left", () => {
+  player.move(-SPEED, 0);
+});
+
+onKeyDown("right", () => {
+  player.move(SPEED, 0);
+});
+
+function spawnLeftBullet(p) {
+  add([
+    rect(12, 48),
+    area(),
+    pos(p),
+    origin("center"),
+    color(255, 0, 0),
+    outline(4),
+    move(UP, BULLET_SPEED),
+    cleanup(),
+    "bullet",
+  ]);
+}
+
+function spawnRightBullet(p) {
+  add([
+    rect(12, 48),
+    area(),
+    pos(p),
+    origin("center"),
+    color(255, 255, 10),
+    outline(4),
+    move(UP, BULLET_SPEED),
+    cleanup(),
+    "bullet",
+  ]);
+}
+
+//user
+let insaneMode = false;
+const BULLET_SPEED = 1200;
+
+onKeyPress("space", () => {
+  spawnLeftBullet(player.pos.sub(16, 0));
+  spawnRightBullet(player.pos.add(16, 0));
+});
+
+onCollide("bullet", "enemy", (b, e) => {
+  destroy(e);
+  addKaboom(e.pos.add(50));
+  destroy(b)
+  shake(10);
+  score.value++;
+  score.text = score.value;
+  cleanup();
+  if (score.value === 7) {
+    destroy("enemy");
+    go("win");
+  }
+});
+
+//enemies
+const ENEMY_SPEED = 300;
+let CURRENT_SPEED = ENEMY_SPEED;
+const speed = 300;
+
+action("enemy", (s) => {
+  s.move(CURRENT_SPEED, 0);
+});
+
+collides("enemy", "topleft", () => {
+  CURRENT_SPEED = ENEMY_SPEED;
+  every("enemy", (e) => {
+    add([
+      pos(e.pos.add(50, 80)),
+      move(DOWN, speed),
+      rect(12, 48),
+      area(),
+      outline(4),
+      cleanup(),
+      origin("center"),
+      color(225, 127, 0),
+      "eBullet",
+    ]);
+  });
+});
+
+collides("enemy", "topright", (e) => {
+  CURRENT_SPEED = -ENEMY_SPEED;
+  every("enemy", (e) => {
+    add([
+      pos(e.pos.add(50, 80)),
+      move(DOWN, speed),
+      rect(12, 48),
+      area(),
+      outline(4),
+      cleanup(),
+      origin("center"),
+      color(225, 127, 0),
+      "eBullet",
+    ]);
+  });
+});
+
+onCollide("eBullet", "player", (e) => {
+  lifeNumber.value--;
+  lifeNumber.value += 0.5;
+  lifeNumber.text = lifeNumber.value;
+  destroy(e);
+  cleanup();
+  if (lifeNumber.value === 0) {
+    destroy(player);
+    go("lose");
+  }
+});
+
+
+//hud
+const scoreHolder = add ([
+  text('score:', {
+      size:40
+  }),
+  pos(width() * .95 ,20),
+  origin("topright"),
+  layer('ui'),
+  
+])
+
+const score = add ([
+  text('0', {
+      size:40
+  }),
+  pos(width() * .96 ,20),
+  origin("topleft"),
+  layer('ui'),
+  {
+      value: 0,
+  }
+])
+
+const livesHolder = add ([
+  text('lives:', {
+      size:40
+  }),
+  pos(width() / 35, 20),
+  layer('ui'),
+])
+
+loadSprite("heart", "./sprites/output.png");
+
+const lifeNumber = add([
+  text("3", {
+      size:45
+  }), // sprite() component makes it render as a sprite
+  pos(width() / 7.3, 17), // pos() component gives it position, also enables movement
+  layer('ui'),
+  {
+      value: 3,
+  }
+]);
+
+const life = add([
+  sprite("heart"), // sprite() component makes it render as a sprite
+  pos(width() / 9.0, 20 ), // pos() component gives it position, also enables movement
+  area(),
+  solid(),
+  "heart"
+]);
+
+})
+
+
 scene("lose", () => {
-  add([text("GAME OVER"), pos(width() / 2, height() / 2)]);
-  origin("center");
+  add([text("GAME OVER"), pos(width() / 2, height() / 2), origin("center")]);
   // // Press any key to go back
   // onKeyPress(start)
 });
 
 scene("win", () => {
-  add([text("You Win"), pos(width() / 2, height() / 2)]);
-  origin("center");
+  add([text("You Win"), pos(width() / 2, height() / 2), origin("center")]);
+  
   // onKeyPress(start)
 })
-
-// function start() {
-// 	// Start with the "game" scene, with initial parameters
-//   add([text("You Win"), pos(width() / 2, height() / 2)]);
-//   origin("center");
-// 	go("game", {
-// 		score: 0,
-// 	})
-// }
-
-// start()
